768e60203fd22b77a3a6801f7b0405de281b6fa4
ALockwood <a.c.lockwood@gmail.com>
2024-06-15 10:13:24 -0400

    Add misc scripts

---
+++ MiscWinScripts/Win-RTC-Clock-to-UTC.ps1
@@ -0,0 +1,2 @@
+# Sets Windows to use UTC as the system clock instead of local time.
+reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\TimeZoneInformation" /v RealTimeIsUniversal /d 1 /t REG_DWORD /f
